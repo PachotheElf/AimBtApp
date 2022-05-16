a42305ac79f39ffee7e802d378a198af92909a3a
Ricardo Rico <ricardo.rico@gmail.com>
2022-05-16 10:27:26 -0400

    Create date.sh

---
+++ date.sh
@@ -0,0 +1,19 @@
+DESTINY=$1
+if [ ! $1 ]; then
+  echo "Warning: No app assigned... Using Dev Android as default"
+  DESTINY=RegattaDevAndroid
+fi
+DEVELOPER=$2
+if [ ! $2 ]; then
+  echo "Warning: No developer assigned... Using Queue as default"
+  DESTINY=Queue
+fi
+
+NOTES=$3
+if [ ! $3 ]; then
+  echo "Warning: No Notes found... Using own's notes (empty)"
+  NOTES=""
+fi
+TIME_STAMP=$(date --rfc-email)
+RELEASE_NOTE="Released on: "
+echo $RELEASE_NOTE$TIME_STAMP
